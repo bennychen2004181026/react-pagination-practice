54a10af7c7140a9e61c92fd84d65221f79de5f95
Ben <ben83026@outlook.com>
2023-10-16 00:44:32 +1100

    create pagination component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import './components/Posts'
 import Post from './components/Posts';
+import Pagination from './components/Pagination';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Post/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Post />
+        <Pagination />
       </header>
     </div>
   );
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,4 @@
+export default function Pagination() {
+    return <div> 123 </div>;
+  }
+  
