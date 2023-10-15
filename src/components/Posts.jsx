70a98cb2bae9e2d09c87e786dac2c2aae3f145e8
Ben <ben83026@outlook.com>
2023-10-16 01:19:15 +1100

    Create fetch posts date and set posts state

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Post() {
+  const [posts, setPosts] = useState([]); //useState
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = "https://jsonplaceholder.typicode.com/posts";
+        const res = await axios.get(url);
+        setPosts(res.data);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []); //componentDidMount
+
   return <ul> Post </ul>;
 }
