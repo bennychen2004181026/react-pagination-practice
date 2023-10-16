7b72bb4dbde70ae87d9f2f63bb942cf07397b1a7
Ben <ben83026@outlook.com>
2023-10-16 13:13:02 +1100

    Move the useState and useEffect to top component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import './App.css';
 import './components/Posts'
 import Post from './components/Posts';
 import Pagination from './components/Pagination';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState([]); //useState
+  const [loading, setLoading] = useState(true); //inital loading state is true
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = "https://jsonplaceholder.typicode.com/posts";
+        const res = await axios.get(url);
+        setLoading(false);
+        setPosts(res.data);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []); //componentDidMount
+
+  if (loading) {
+    return <div> Loading... </div>;
+  }
   return (
     <div className="App">
 
